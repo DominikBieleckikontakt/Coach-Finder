89bf051ac45d325a0893a8eac90c060f51e8657c
Dominik <dominikbielecki.kontakt@gmail.com>
2025-08-23 09:34:09 +0200

    Added requests connected with DB, added error handling and loaders

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,16 +1,22 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <coach-filter @change-filter="setFilters"></coach-filter>
   </section>
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
-        <base-button link to="/register" v-if="!isCoach"
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
+        <base-button link to="/register" v-if="!isCoach && !isLoading"
           >Register as Coach</base-button
         >
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -37,6 +43,8 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
+      error: null,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -61,7 +69,7 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters["coaches/hasCoaches"];
+      return !this.isLoading && this.$store.getters["coaches/hasCoaches"];
     },
 
     isCoach() {
@@ -72,6 +80,21 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
+    async loadCoaches() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch("coaches/loadCoaches");
+      } catch (error) {
+        this.error = error.message || "Something went wrong!";
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
+  },
+  created() {
+    this.loadCoaches();
   },
 };
 </script>
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,63 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit("registerCoach", coachData);
+
+    const response = await fetch(
+      `https://coach-finder-820bb-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const resData = await response.json();
+
+    if (!response.ok) {
+      // error ...
+    }
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      "https://coach-finder-820bb-default-rtdb.firebaseio.com/coaches.json"
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(data.message || "Failed to fetch!");
+      throw error;
+    }
+
+    const coaches = [];
+    for (const key in data) {
+      const coach = {
+        id: key,
+        firstName: data[key].firstName,
+        lastName: data[key].lastName,
+        description: data[key].description,
+        hourlyRate: data[key].hourlyRate,
+        areas: data[key].areas,
+      };
+
+      coaches.push(coach);
+    }
+
+    context.commit("setCoaches", coaches);
   },
 };
